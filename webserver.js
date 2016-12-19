e61044801c50cada435af646ae2611b6440ce50f
coyotte508 <coyotte508@gmail.com>
2016-12-19 16:01:14 +0100

    Learn sequences

--- webserver.js
+++ webserver.js
@@ -2,10 +2,18 @@ const express = require('express');
 const path = require('path');
 const compression = require('compression');
 const bodyParser = require('body-parser');
+const assert = require('assert');
+const spawn = require('child_process').spawn;
+const co = require('co');
+const delay = require('./delay.js');
 
 var config = {
   number: "",
-  lastWords: []
+  lastWords: [],
+  wordsRemaining: 0,
+  status: "done",
+  opCount: 0,
+  decomposed: ""
 };
 
 const app = express();
@@ -25,3 +33,67 @@ app.get('/', function(req, res) {
 });
 
 app.listen(3508);
+
+var chunks = "";
+
+/* Run the sequence as a subprocess */
+const seq = spawn('./sequence');
+
+function resetChunks() {
+  chunks = "";
+  config.opCount += 1;
+}
+
+seq.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+
+  chunks += data;
+
+  if (chunks.includes("Input a word to have it decomposed")) {
+    chunks = chunks.split("\n");
+    config.lastWords = chunks.filter((str) => str.startsWith("- ")).map((str) => str.substr(2).trim());
+    resetChunks();
+  }
+  if (chunks.trim().endsWith(")")) {
+    chunks = chunks.split("\n").pop().trim();
+    config.decomposed = chunks.substr(0, chunks.lastIndexOf(" "));
+    resetChunks();
+  }
+});
+
+seq.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+seq.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+function *exec(arg) {
+  var count = config.opCount;
+  seq.stdin.write(arg + "\n");
+
+  while (config.opCount == count || config.status != "done") {
+    yield delay(50);
+  }
+}
+
+app.post("/", function(req, res) {
+  console.log(JSON.stringify(req.body));
+
+  co(function*() {
+    var num = Number((req.body["number-seq"]).trim());
+    assert(Number.isInteger(num) && num > 0 && num < 60000, "The number provided must be an integer between 1 and 50000");
+
+    yield exec(num);
+
+    console.log("no errors!");
+
+    config.number = num;
+    res.render('index', {config, error:null});
+  }).catch((err) => {
+    console.log("sending back error", err);
+    res.status(500);
+    res.render('index', {config, error: err.message});
+  });
+});
